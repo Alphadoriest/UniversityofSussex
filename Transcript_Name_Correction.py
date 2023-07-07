f5bbc14142a2378973430f3e8ad40e1eda28860a
Alphadoriest <138936401+Alphadoriest@users.noreply.github.com>
2023-07-07 22:57:44 +0100

    Update Transcript_Name_Correction.py

--- Transcript_Name_Correction.py
+++ Transcript_Name_Correction.py
@@ -1,6 +1,15 @@
 import re
+import requests
+import PyPDF2
+import docx
+import nltk
+from bs4 import BeautifulSoup
 from difflib import SequenceMatcher
+from io import BytesIO, StringIO
 import streamlit as st
+
+nltk.download('names')
+nltk.download('punkt')
 
 def similarity(a, b):
     return SequenceMatcher(None, a, b).ratio()
@@ -25,10 +34,58 @@ def replace_similar_names(text, names_list):
 
     return replaced_names, text
 
+def read_pdf(file):
+    pdf_reader = PyPDF2.PdfFileReader(file)
+    text = ''
+    for page_num in range(pdf_reader.numPages):
+        text += pdf_reader.getPage(page_num).extractText()
+    return text
+
+def read_docx(file):
+    doc = docx.Document(file)
+    text = ''
+    for paragraph in doc.paragraphs:
+        text += paragraph.text
+    return text
+
+def read_html(content):
+    soup = BeautifulSoup(content, 'html.parser')
+    text = soup.get_text(separator='\n')
+    return text
+
+def extract_names(text):
+    words = nltk.word_tokenize(text)
+    names = set()
+    for word in words:
+        if word.istitle() and word.lower() not in nltk.corpus.names.words('male.txt') + nltk.corpus.names.words('female.txt'):
+            names.add(word)
+    return ', '.join(names)
+
 st.title("Name Replacer")
 
 names_input = st.text_input("Enter names to match (separated by commas):")
 text_input = st.text_area("Enter text:")
+
+names_file = st.file_uploader("Upload names list file (PDF, DOCX, or URL):", type=['pdf', 'docx', 'txt', 'html'])
+text_file = st.file_uploader("Upload text file (PDF, DOCX, or URL):", type=['pdf', 'docx', 'txt', 'html'])
+
+if names_file:
+    if names_file.type == 'application/pdf':
+        names_text = read_pdf(names_file)
+    elif names_file.type == 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
+        names_text = read_docx(names_file)
+    elif names_file.type in ['text/plain', 'text/html']:
+        names_text = names_file.getvalue().decode('utf-8')
+
+    names_input = extract_names(names_text)
+
+if text_file:
+    if text_file.type == 'application/pdf':
+        text_input = read_pdf(text_file)
+    elif text_file.type == 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
+        text_input = read_docx(text_file)
+    elif text_file.type in ['text/plain', 'text/html']:
+        text_input = text_file.getvalue().decode('utf-8')
 
 if st.button("Submit"):
     names_list = [name.strip() for name in names_input.split(',')]
