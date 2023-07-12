ff2efd39f9791c1f29b966fc9fdbaec65179f4a5
Alphadoriest <138936401+Alphadoriest@users.noreply.github.com>
2023-07-12 19:24:42 +0100

    Update Transcript_Name_Correction.py

--- Transcript_Name_Correction.py
+++ Transcript_Name_Correction.py
@@ -5,7 +5,9 @@ from difflib import SequenceMatcher
 import streamlit as st
 from streamlit.components.v1 import html
 from docx import Document
+
 # Name Extractor for graduation ceremony in-person lists functions
+
 def extract_middle_column_text(doc):
     middle_column_texts = []
     for table in doc.tables:
@@ -49,35 +51,48 @@ def decapitalize(text):
             apostrophe_parts = [part.lower().title() for part in apostrophe_parts]
             words[i] = "'".join(apostrophe_parts)
     return ' '.join(words)
+    
 st.header('Graduation Transcription Workflow Web Tool')
 # Add the banner image at the top of the app
 st.image("banner.jpg")
+
 # Name Extractor UI
 st.title('Name Extractor for Graduation Ceremony In-Person Lists')
 uploaded_file = st.file_uploader("Choose a Word document", type="docx")
+
 # Initialize names_list as an empty string
+
 names_list = ''
 if uploaded_file is not None:
     document = Document(io.BytesIO(uploaded_file.read()))
     names_list = extract_middle_column_text(document)  # Keep names_list as a list
+
 # Use names_list as the default value for the names_list text_area
+
 names_list = st.text_area("Enter names (separate names with commas):", ', '.join(names_list), key='names_list')
 if names_list:  # Check if names_list is not empty
     names_list = names_list.split(',')
     names_list = [name.strip() for name in names_list]
+
 #Correct all names in graduation transcript (find and replace) functions
+
 def similarity(a, b):
     return SequenceMatcher(None, a, b).ratio()
+    
 def replace_similar_names(text, names_list):
     full_name_pattern = re.compile(r'(?<!:)(?:\b\w+(?:\s+\w+){1,4}\b)(?!\d)')
     replaced_names = []
     lines = text.split('\n')
     processed_lines = []
+
     def replace_name(match):
         full_name = match.group(0)
         max_similarity = 0
         most_similar_name = None
         for name in names_list:
+            # Add a condition to check word count
+            if len(full_name.split()) < len(name.split()):
+                continue
             sim = similarity(full_name, name)
             if sim > max_similarity:
                 max_similarity = sim
@@ -89,6 +104,7 @@ def replace_similar_names(text, names_list):
             return most_similar_name
         else:
             return full_name
+
     for line in lines:
         # Skip timecode lines
         if re.match(r'\d\d:\d\d:\d\d\.\d\d\d\s*-->', line):
@@ -101,8 +117,11 @@ def replace_similar_names(text, names_list):
     # Remove leading whitespaces from all lines as a final step
     new_text = '\n'.join(line.lstrip() for line in new_text.split('\n'))
     return replaced_names, new_text
+    
 #Name Corrector UI
+
 st.title("Graduation Transcript Name Corrector")
+
 # Initialize transcript_text as an empty string
 transcript_text = ''
 uploaded_transcript_file = st.file_uploader("Choose a VTT or text document for transcript", type=["vtt", "txt"])
