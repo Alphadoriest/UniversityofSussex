852a82e5d0f62af6fae2c9bfbe9e30ebf475400d
Alphadoriest <138936401+Alphadoriest@users.noreply.github.com>
2023-07-14 02:29:40 +0100

    Update Transcript_Name_Correction.py

--- Transcript_Name_Correction.py
+++ Transcript_Name_Correction.py
@@ -46,10 +46,6 @@ def extract_middle_column_text(doc):
 # Initialize names_list as an empty string
 names_list = ''
 
-if uploaded_file is not None:
-    document = Document(io.BytesIO(uploaded_file.read()))
-    names_list = extract_middle_column_text(document)  # Keep names_list as a list
-
 # Use names_list as the default value for the names_list text_area
 names_list = st.text_area("Enter names (separate names with commas):", ', '.join(names_list), key='names_list')
 
@@ -162,6 +158,10 @@ st.image("banner.jpg")
 st.title('Name Extractor for Graduation Ceremony In-Person Lists')
 
 uploaded_file = st.file_uploader("Choose a Word document", type="docx")
+
+if uploaded_file is not None:
+    document = Document(io.BytesIO(uploaded_file.read()))
+    names_list = extract_middle_column_text(document)  # Keep names_list as a list
 
 st.title("Graduation Transcript Name Corrector")
 # Initialize transcript_text as an empty string
