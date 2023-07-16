e2a146f9b9487f3df2d4fcdbca62e8d7eeedabcf
Alphadoriest <138936401+Alphadoriest@users.noreply.github.com>
2023-07-16 01:33:43 +0100

    Update Transcript_Name_Correction.py

--- Transcript_Name_Correction.py
+++ Transcript_Name_Correction.py
@@ -211,8 +211,8 @@ if uploaded_file is not None:
     document = Document(io.BytesIO(uploaded_file.read()))
     names_list = extract_middle_column_text(document)  # Keep names_list as a list
 
-    # Use names_list as the default value for the names_list text_area
-    names_list = st.text_area("Enter names, separated by commas:", ', '.join(names_list), key='names_list')
+# Use names_list as the default value for the names_list text_area
+names_list = st.text_area("Enter names, separated by commas:", ', '.join(names_list), key='names_list')
 
 if names_list:  # Check if names_list is not empty
     names_list = names_list.split(',')
