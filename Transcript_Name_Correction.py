825f6b85a33463b0ba1dcb7f74bfadb30d3e67da
Alphadoriest <138936401+Alphadoriest@users.noreply.github.com>
2023-07-16 01:49:17 +0100

    Update Transcript_Name_Correction.py

--- Transcript_Name_Correction.py
+++ Transcript_Name_Correction.py
@@ -217,6 +217,8 @@ names_list = st.text_area("Enter names, separated by commas:", ', '.join(names_l
 if names_list:  # Check if names_list is not empty
     names_list = names_list.split(',')
     names_list = [name.strip() for name in names_list]
+    # Check if names_list contains meaningful entries
+    if any(name for name in names_list):
 
 # Assuming format_names now returns a list of tuples like [(name, color), ...]
 formatted_names = format_names(names_list)
