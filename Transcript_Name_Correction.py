95486707602e58bc0b7625b4832228a6fff9400a
Alphadoriest <138936401+Alphadoriest@users.noreply.github.com>
2023-07-20 12:06:32 +0100

    Update Transcript_Name_Correction.py

--- Transcript_Name_Correction.py
+++ Transcript_Name_Correction.py
@@ -312,7 +312,18 @@ def reformat_subtitles(text: str) -> str:
         formatted_blocks.append(formatted_block)
         print(f"After: {formatted_block}")
 
-        print(f"formatted_blocks: {formatted_blocks}")
+        # Create a list of lines from formatted_blocks
+lines = ''.join(formatted_blocks).split('\n')
+
+# Find the index of the line containing "Dain Jeong"
+          index = next((i for i, line in enumerate(lines) if "Dain Jeong" in line), -1)
+          
+          # Print a few lines before and after the line containing "Dain Jeong"
+          if index != -1:
+              start = max(0, index - 5)  # 5 lines before, or start of text if less than 5 lines
+              end = min(len(lines), index + 6)  # 5 lines after, or end of text if less than 5 lines
+              for i in range(start, end):
+                  print(lines[i])
 
     return ''.join(formatted_blocks)  # Return as a string
 
