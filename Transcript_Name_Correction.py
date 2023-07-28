b5fba5b440aa3060627c7985a69e132dbf3dbb64
Alphadoriest <138936401+Alphadoriest@users.noreply.github.com>
2023-07-28 12:29:43 +0100

    Update Transcript_Name_Correction.py

--- Transcript_Name_Correction.py
+++ Transcript_Name_Correction.py
@@ -436,7 +436,7 @@ if names_list:  # Check if names_list is not empty
         formatted_names = format_names(names_list)
         
         # Create the names list as a Markdown string
-        names_md = ', '.join([f'<span style="color:{color};"><strong><u>{name}</u></strong></span>' if len(name.split()) > 4 or len(name.split()) < 2 or any(len(word) < 3 for word in name.split()) or (re.search(r'[^a-zA-Z\s]', name) and not name.endswith('(Marked As Not Present)')) else f'<span style="color:{color};">{name}</span>' for name, color in formatted_names])
+        names_md = ', '.join([f'<span style="color:{color};"><strong><u>{name}</u></strong></span>' if len(name.split()) > 4 or len(name.split()) < 2 or any(len(word) < 3 for word in name.split()) or (re.search(r'[^a-zA-Z\s]', name) and '(Marked As Not Present)' not in name) else f'<span style="color:{color};">{name}</span>' for name, color in formatted_names])
         
         # Display the names list using st.markdown
         st.markdown(names_md, unsafe_allow_html=True)
