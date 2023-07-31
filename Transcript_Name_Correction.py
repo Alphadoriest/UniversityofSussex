0998fdc9076c4a5977b2be956883413c467aa110
Alphadoriest <138936401+Alphadoriest@users.noreply.github.com>
2023-07-31 21:03:29 +0100

    Update Transcript_Name_Correction.py

--- Transcript_Name_Correction.py
+++ Transcript_Name_Correction.py
@@ -132,6 +132,7 @@ def extract_middle_column_text(doc):
                 middle_cell = cells[len(cells) // 2]
                 paragraphs = middle_cell.paragraphs
                 for paragraph in paragraphs:
+                    paragraph_text = ' '.join(run.text for run in paragraph.runs)
                     clean_paragraph_text = ''
                     for run in paragraph.runs:
                         if run.font.strike:  # Check if the text is strikethrough
@@ -141,14 +142,23 @@ def extract_middle_column_text(doc):
                         else:
                             clean_paragraph_text += run.text + ' '
 
-                    # Remove brackets from the whole paragraph
-                    clean_paragraph_text = regex.sub(r'(?s)\((?:[^()]|(?R))*\)', '', clean_paragraph_text)  # Recursive regex to remove all round bracketed text
-                    clean_paragraph_text = regex.sub(r'(?s)\[(?:[^\[\]]|(?R))*\]', '', clean_paragraph_text)  # Recursive regex to remove all square bracketed text
+                    if paragraph_text.strip().lower().startswith('also awarded the'):
+                        # If the text starts with "Also awarded the", store the whole paragraph text
+                        middle_column_texts.append(paragraph_text)
+                    else:
+                        # Remove brackets from the whole paragraph
+                        clean_paragraph_text = regex.sub(r'(?s)\((?:[^()]|(?R))*\)', '', clean_paragraph_text)  # Recursive regex to remove all round bracketed text
+                        clean_paragraph_text = regex.sub(r'(?s)\[(?:[^\[\]]|(?R))*\]', '', clean_paragraph_text)  # Recursive regex to remove all square bracketed text
 
-                    # Add the cleaned text to the list
-                    if clean_paragraph_text.strip():
-                        middle_column_texts.append(clean_paragraph_text.strip())
+                        # Add the cleaned text to the list
+                        if clean_paragraph_text.strip():
+                            middle_column_texts.append(clean_paragraph_text.strip())
 
+    # If any text starts with "Also awarded the", only keep the last paragraph
+    for i in range(len(middle_column_texts)):
+        if middle_column_texts[i].strip().lower().startswith('also awarded the'):
+            middle_column_texts[i] = middle_column_texts[i].split('\n')[-1]  # Keep only the last line
+          
     cleaned_text = re.sub(r'(,\s*)+', ', ', ', '.join(middle_column_texts))  # Replace multiple commas with a single comma
     # Remove single letters from names
     cleaned_names = []
