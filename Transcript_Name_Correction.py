113badc096cde874ac5e0d0408c2ad75920aa0da
Alphadoriest <138936401+Alphadoriest@users.noreply.github.com>
2023-07-31 05:15:11 +0100

    Update Transcript_Name_Correction.py

--- Transcript_Name_Correction.py
+++ Transcript_Name_Correction.py
@@ -133,26 +133,22 @@ def extract_middle_column_text(doc):
                 paragraphs = middle_cell.paragraphs
                 desired_text = ''
                 for paragraph in paragraphs:
-                    clean_paragraph_text = ''
                     for run in paragraph.runs:
                         lines = run.text.split('\n')
                         for line in lines:
                             line = line.strip()
-                            # Ignore lines that are fully enclosed in brackets
-                            if not (line.startswith('(') and line.endswith(')')) and not (line.startswith('[') and line.endswith(']')):
+                            # Remove lines that are fully enclosed in brackets
+                            line = regex.sub(r'^\((?:[^()]|(?R))*\)$', '', line)  # Recursive regex to remove all round bracketed text
+                            line = regex.sub(r'^\[(?:[^\[\]]|(?R))*\]$', '', line)  # Recursive regex to remove all square bracketed text
+                            if line:
                                 if run.font.strike:  # Check if the text is strikethrough
-                                    clean_paragraph_text += '~~' + line + '~~\n'
+                                    desired_text += '~~' + line + '~~\n'
                                 else:
-                                    clean_paragraph_text += line + '\n'
+                                    desired_text += line + '\n'
                     
-                    lines = clean_paragraph_text.split('\n')
-                    for line in lines:
-                        line = line.strip()
-                        if line:
-                            # Ignore lines that are fully enclosed in brackets
-                            if not (line.startswith('(') and line.endswith(')')) and not (line.startswith('[') and line.endswith(']')):
-                                desired_text = line
-                middle_column_texts.append(desired_text)
+                desired_text = desired_text.strip()
+                if desired_text:
+                    middle_column_texts.append(desired_text)
 
     cleaned_text = re.sub(r'(,\s*)+', ', ', ', '.join(middle_column_texts))  # Replace multiple commas with a single comma
 
