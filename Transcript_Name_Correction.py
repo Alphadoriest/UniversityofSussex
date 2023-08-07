a14957369c2b9b1cb2f5963df5066773a5da849d
Alphadoriest <138936401+Alphadoriest@users.noreply.github.com>
2023-08-07 17:38:14 +0100

    Update Transcript_Name_Correction.py

--- Transcript_Name_Correction.py
+++ Transcript_Name_Correction.py
@@ -133,7 +133,7 @@ def extract_names(doc):
     excluded_phrases_regex = re.compile("|".join(excluded_phrases), re.IGNORECASE)
 
     # Regular expression for name extraction
-    name_regex = re.compile(r'\b[A-Z][a-zA-Z]*(?:\s[A-Z][a-zA-Z]*)*\b')
+    name_regex = re.compile(r'[A-Z][a-zA-Z]*(?:\s+[A-Z][a-zA-Z]*)*')
 
     # Iterate over all tables and rows
     for table in doc.tables:
