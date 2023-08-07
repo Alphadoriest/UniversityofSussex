6586f8bc7dc9aa349a3e88af19ae6d816d8fe06c
Alphadoriest <138936401+Alphadoriest@users.noreply.github.com>
2023-08-07 03:50:00 +0100

    Update Transcript_Name_Correction.py

--- Transcript_Name_Correction.py
+++ Transcript_Name_Correction.py
@@ -146,12 +146,18 @@ def extract_names(doc):
                     # Check if the text is strikethrough
                     is_strikethrough = any(run.font.strike for run in paragraph.runs)
 
-                    # Check if the text is not in excluded phrases and ends with a uppercase word
-                    if not any(excluded_phrase in text for excluded_phrase in excluded_phrases) and re.search(r'\b[A-Z]+\b$', text):
-                        # This line contains the name. 
-                        # Add note if name is strikethrough
-                        if is_strikethrough:
-                            text += ' (Marked As Not Present)'
+                    if not any(excluded_phrase in text for excluded_phrase in excluded_phrases):
+                        # Check if there is only one line and the last word isn't capitalized
+                        if len(paragraphs) == 1 and not text.split()[-1].isupper():
+                            # Capitalize the last word and extract the line
+                            text = ' '.join(text.split()[:-1]) + ' ' + text.split()[-1].upper()
+                            
+                        elif re.search(r'\b[A-Z]+\b$', text):
+                            # This line contains the name. 
+                            # Add note if name is strikethrough
+                            if is_strikethrough:
+                                text += ' (Marked As Not Present)'
+                            
                         middle_column_texts.append(text)
 
     # Decapitalize names before returning
