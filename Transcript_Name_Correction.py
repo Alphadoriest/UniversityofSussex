1c16522554f335f727c2784325adbe5dd67d3542
Alphadoriest <138936401+Alphadoriest@users.noreply.github.com>
2023-08-09 13:08:25 +0100

    Update Transcript_Name_Correction.py

--- Transcript_Name_Correction.py
+++ Transcript_Name_Correction.py
@@ -547,8 +547,8 @@ for original, replaced, similarity in sorted(st.session_state.replaced_names, ke
          replaced_words = replaced.split()
          if len(original_words) != len(replaced_words):
             st.markdown(f"**{original} -> {replaced} (Similarity: {similarity:.2f})**")
-                else:
-                    st.write(f"{original} -> {replaced} (Similarity: {similarity:.2f})")
+        else:
+            st.write(f"{original} -> {replaced} (Similarity: {similarity:.2f})")
             
             st.subheader("Names not matched:")
             st.text("These can be addressed in one of two ways. Either copy the comma separated list and run just those names in another instance of the app at a lower threshold or browser search for the names surrounding the unmatched name and paste in the correct name in the updated subtitles text box. The app will reset after each addition, but all progress is saved.")
