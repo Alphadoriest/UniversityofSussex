983475c6b2eaa0574faae2f228febd938eadd7ff
Alphadoriest <138936401+Alphadoriest@users.noreply.github.com>
2023-08-14 01:26:13 +0100

    Update Transcript_Name_Correction.py

--- Transcript_Name_Correction.py
+++ Transcript_Name_Correction.py
@@ -493,10 +493,10 @@ with st.expander("2 - Name Extractor for Graduation Ceremony In-Person Lists"):
             formatted_names = format_names(names_list)
         
         # Create the names list as a Markdown string
-        names_md = ', '.join(
+names_md = ', '.join(
     [
         f'<span style="color:{color};"><strong><u>{name}</u></strong></span>'
-        if (len(name.split()) > 7 or re.search(r'[()\[\]{}]', name))
+        if (len(name.split()) > 7 or re.search(r'[()\[\]{}\'",;&*=+/?::-\.]', name))
         else f'<span style="color:{color};">{name}</span>'
         for name, color in formatted_names
     ]
